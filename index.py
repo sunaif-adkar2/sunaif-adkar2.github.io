b38eaef4e07fb4df7bc4eede353d6f59dcbc8b65
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-11-07 15:40:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,7 +5,7 @@
 import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
 
-mobileNo=int("Mobile no. with country code:")
+mobileNo=input("Mobile no. with country code:")
 mobileNo=phonenumbers.parse(mobileNo)
 print(timezone.time_zones_for_number(mobileNo))
 print(carrier.name_for_number(mobileNo,"en"))
